0b7d4989e531e401e0169d52a688a93e59f2cd97
Student <student@compass.local>
2026-03-17 12:41:39 +0600

    classwork

---
+++ src/components/dish-card/DishCard.tsx
@@ -0,0 +1,36 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import type { IDish } from '../../types';
+import { Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router';
+
+interface Props {
+    dish: IDish
+}
+
+export const DishCard = ({dish}:Props) => {
+    const navigate = useNavigate()
+
+    const goToDishPage = (id: string) => {
+        navigate(`/dish/${id}`)
+    }
+
+    return(
+        <Card>
+            <CardContent>
+                <Typography variant='body1'>
+                    {dish.name}
+                </Typography>
+                <Typography variant='body2'>
+                    price: {dish.price} $
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button onClick={()=> goToDishPage(dish.id)}>
+                    Show more
+                </Button>
+            </CardActions>
+        </Card>
+    )
+}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface IDishShort {
+  name: string
+  description: string
+  price: number
+}
+
+export interface IDishList{
+  [id:string]: IDishShort
+}
+
+export interface IDish extends IDishShort{
+  id: string
+}
