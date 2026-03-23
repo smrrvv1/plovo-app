12881c798e92bc2b011e331c237a0ba23eb0326a
smrrvv1 <samira.b@compasscollege.art>
2026-03-24 01:24:45 +0600

    upd routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from './pages/home/Home'
 import { Header } from './components/header/Header'
 import { AddDish } from './pages/add-dish/AddDish'
 import { Container } from '@mui/material'
+import { EditDish } from './pages/edit-dish/EditDish'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/dish/:id' element={<Dish/>}/>
         <Route path='/dish/create' element={<AddDish/>}/>
+        <Route path='/dish/edit/:id' element={<EditDish/>}/>
       </Routes>
     </Container>
     </>
